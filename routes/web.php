e9528b0d1bb162600c27b6f4ac2a86265fd43bc4
jhsc1988 <jhsc1988@gmail.com>
2023-07-18 15:46:03 +0200

    basic layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// All Listings
+Route::get('/', [PageController::class, 'index']);
