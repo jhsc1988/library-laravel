7c9baf3ed83058f26910fd1a46bc6cc1504935a6
jhsc1988 <jhsc1988@gmail.com>
2023-07-19 21:37:49 +0200

    add users for seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Jasmin',
+            'surname' => 'Husić',
+            'email' => 'admin@library.com',
+            'role'=> 'admin',
+            'password'=> 'adminpass1234'
+        ]);
+
+        User::create([
+            'name' => 'Hrvoje',
+            'surname' => 'Horvat',
+            'email' => 'user@library.com',
+            'role'=> 'user',
+            'password'=> 'userpass1234'
+        ]);
     }
 }
