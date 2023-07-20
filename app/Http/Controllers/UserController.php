bb00d197c578d18f1053f72d18c445e4006e034f
jhsc1988 <jhsc1988@gmail.com>
2023-07-20 14:03:39 +0200

    surname required fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
     public function store(Request $request){
         $formFields = $request->validate([
             'name'=>['required', 'min:3'],
-            'surname'=>['required, min:3'],
+            'surname'=>['required', 'min:3'],
             'email'=>['required', 'email', Rule::unique('users', 'email')],
             'password'=>['required', 'confirmed','min:6']
         ]);
