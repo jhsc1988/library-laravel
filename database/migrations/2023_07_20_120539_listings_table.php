bc8b799b16ee0d2fa0110697e20e56d309d2e0c7
jhsc1988 <jhsc1988@gmail.com>
2023-07-20 15:13:24 +0200

    add listings migration and model

---
+++ database/migrations/2023_07_20_120539_listings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->string('language');
+            $table->text('annotation');
+            $table->string('ISBN');
+            $table->string('cover')->nullable();
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
