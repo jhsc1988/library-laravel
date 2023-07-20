2c92cc4d91fdf88d649030eba9b86869bd233fdd
jhsc1988 <jhsc1988@gmail.com>
2023-07-20 14:03:20 +0200

    add surname fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
     ];
@@ -42,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
 }
