a02c75661f04912f79d081a583d509ecc44dcfd0
jhsc1988 <jhsc1988@gmail.com>
2023-07-23 17:07:08 +0200

    show buttons

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="mx-96 p-10 ">
+    <div class="mx-96 p-10 mt-16">
         <x-card class="p-10">
             <div class="text-sm text-left">
                 <a href="/" class="ext-black "><i class="fa-solid fa-arrow-left"></i> Povratak</a>
@@ -17,10 +17,20 @@
                 </blockquote>
 
                 <hr/>
-                <div class="border-b-4 p-4 border-gray-200 w-full mb-6"></div>
+                <div class="border-b p-4 border-gray-200 w-full mb-6"></div>
                 <div>
-                    <div class="p-4 text-zinc-900 dark:text-sky-400">
-                        Dostupno:
+                    <div>
+                        <b>Nakladnik:</b> {{$listing->publisher}}
+                    </div>
+                    <div>
+                        <b>Jezik:</b> {{$listing->language}}
+                    </div>
+                    <div>
+                        <b>ISBN:</b> {{$listing->isbn}}
+                    </div>
+                    <div class="border-b p-4 border-gray-200 w-full mb-6"></div>
+                    <div class="text-zinc-900 dark:text-sky-400">
+                        <b>Dostupno:</b>
                         @php
                             if($listing->amount == 0)
                                 echo('<span style="color: red;"> 0</span>');
@@ -28,10 +38,48 @@
                             echo($listing->amount);
                         @endphp
                     </div>
-                    <div class="text-lg space-y-6">{{$listing->description}}
-                        <a href="{{$listing->website}}" target="_blank"
-                           class="block bg-black text-white py-2 rounded-xl hover:opacity-80">
-                            <i class="fa-solid fa-globe"></i> Posudi</a>
+                    <div class="border-b p-4 border-gray-200 w-full mb-6"></div>
+                    <div class="flex justify-center items-center">
+                        <button type="submit"
+                                class="relative inline-flex items-center justify-center w-auto px-8 py-2 overflow-hidden font-bold text-red-500 transition-all duration-500 border border-gray-200 rounded-md cursor-pointer group ease bg-gradient-to-b from-white to-gray-50 hover:from-gray-50 hover:to-white active:to-white">
+                            <span
+                                class="w-full h-0.5 absolute bottom-0 group-active:bg-transparent left-0 bg-gray-100"></span>
+                            <span
+                                class="h-full w-0.5 absolute bottom-0 group-active:bg-transparent right-0 bg-gray-100"></span>
+                            <i class="fa-solid fa-book-open-reader"></i> &nbsp Posudi
+                        </button>
+
+                        <?php if (auth()->check() && auth()->user()->isAdmin()): ?>
+                        &nbsp; &nbsp; &nbsp;
+
+                        <form method="GET" action="/listings/<?php echo $listing->id; ?>/edit">
+                                <?php echo csrf_field(); ?>
+                            <button type="submit"
+                                    class="relative inline-flex items-center justify-center w-auto px-8 py-2 overflow-hidden font-bold text-red-500 transition-all duration-500 border border-gray-200 rounded-md cursor-pointer group ease bg-gradient-to-b from-white to-gray-50 hover:from-gray-50 hover:to-white active:to-white">
+                                <span
+                                    class="w-full h-0.5 absolute bottom-0 group-active:bg-transparent left-0 bg-gray-100"></span>
+                                <span
+                                    class="h-full w-0.5 absolute bottom-0 group-active:bg-transparent right-0 bg-gray-100"></span>
+                                <i class="fa-solid fa-pen-to-square"></i> &nbsp; Promijeni
+                            </button>
+                        </form>
+
+                        &nbsp; &nbsp; &nbsp;
+
+                        <form method="POST" action="/listings/<?php echo $listing->id; ?>">
+                                <?php echo csrf_field(); ?>
+                                <?php echo method_field('delete'); ?>
+                            <button type="submit"
+                                    class="relative inline-flex items-center justify-center w-auto px-8 py-2 overflow-hidden font-bold text-red-500 transition-all duration-500 border border-gray-200 rounded-md cursor-pointer group ease bg-gradient-to-b from-white to-gray-50 hover:from-gray-50 hover:to-white active:to-white">
+                                <span
+                                    class="w-full h-0.5 absolute bottom-0 group-active:bg-transparent left-0 bg-gray-100"></span>
+                                <span
+                                    class="h-full w-0.5 absolute bottom-0 group-active:bg-transparent right-0 bg-gray-100"></span>
+                                <i class="fa-solid fa-trash"></i> &nbsp; Briši
+                            </button>
+                        </form>
+                        <?php endif; ?>
+
                     </div>
                 </div>
             </div>
